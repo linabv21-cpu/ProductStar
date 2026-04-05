836fba8bf270733bf66ab427415a7a87ab7dc24a
Галина Бойко <galinaboiko@MacBook-Pro-Galina.local>
2026-04-05 23:32:46 +0300

    MusicPlaylist

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+public static void main(String[] args) {
+    MusicPlaylist playlist = new MusicPlaylist();
+
+    playlist.addSong("Луна");
+    playlist.addSong("Камин");
+    playlist.addSong("Ламбада");
+    playlist.addSong("Наоборот");
+    playlist.addSong("Солнце");
+
+    System.out.println("=== Плейлист после добавления ===");
+    playlist.printPlayList();
+
+    //Переместить песню
+    System.out.println("\n=== Перемещаем песню 1 на позицию 3 ===");
+    playlist.moveSong(1,3);
+    playlist.printPlayList();
+    //Перемешиваем песню
+    System.out.println("\n=== Перемешиваем ===");
+    playlist.shuffle();
+    playlist.printPlayList();
+
+    //Удалить песню
+    System.out.println("\n===Удаляем песню №1===");
+    playlist.removeSong(1);
+    playlist.printPlayList();
+
+    //Выводим кол-во песен
+    System.out.println("\nВсего песен: " + playlist.getSongCount());
+
+        }
+    }
--- src/MusicPlaylist.java
+++ src/MusicPlaylist.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class MusicPlaylist {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MusicPlaylist {
+    private ArrayList<String> songs;
+    //private - Доступно только внутри этого класса (инкапсуляция!)
+
+    //Конструктор
+    public MusicPlaylist(){
+    songs = new ArrayList<>();
+
+}
+//Метод 1 ддобавить песню
+    public void addSong(String title) {
+        songs.add(title);
+    }
+    //Метод 2 удалить песню
+    public void  removeSong(int index) {
+        if(index >= 0 && index < songs.size()) {
+            songs.remove(index);
+        }else{
+            System.out.println("Неверный индекс!");
+        }
+    }
+    //Метод 3 переместить песню
+    public void moveSong(int from, int to) {
+        if (from >= 0 && from < songs.size()
+                && to >= 0 && to < songs.size()) {
+            String song = songs.get(from);
+            songs.remove(from);
+            songs.add(to, song);
+        }else {
+            System.out.println("Неверные индексы!");
+        }
+    }
+        public void shuffle() {
+            Collections.shuffle(songs);
+        }
+        public void printPlayList() {
+            if (songs.isEmpty()) {
+                System.out.println("Плейлист пуст" );
+                return;
+            }
+            for(int i = 0; i < songs.size(); i++) {
+                System.out.println((i+1) +". " + songs.get(i));
+            }
+
+        }
+
+        public int getSongCount() {
+            return songs.size();
+        }
+
+
+
 }
